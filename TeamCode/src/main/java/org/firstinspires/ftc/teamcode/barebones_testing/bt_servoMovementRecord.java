36c8a4d6f716039157a36f9d96154371fc743e73
C987engineer <c987engineer@gmail.com>
2016-12-10 00:30:12 -0500

    WOOHOO.
    Man that took a while, but I figured out how to create and write to a
    public text file. I have yet to read from it, but here's the code so
    far.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/barebones_testing/bt_servoMovementRecord.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/barebones_testing/bt_servoMovementRecord.java
@@ -3,10 +3,51 @@ package org.firstinspires.ftc.teamcode.barebones_testing;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.exception.FTDeviceClosedException;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.TouchSensor;
 import com.qualcomm.robotcore.util.ElapsedTime;
+import java.io.Console;
+import java.io.File;
+
+import android.app.Application;
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.IntentSender;
+import android.content.ServiceConnection;
+import android.content.SharedPreferences;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.content.res.AssetManager;
+import android.content.res.Configuration;
+import android.content.res.Resources;
+import android.database.DatabaseErrorHandler;
+import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.UserHandle;
+import android.support.annotation.Nullable;
+import android.view.Display;
+
+import org.firstinspires.ftc.robotcontroller.internal.FtcRobotControllerActivity;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
 
 import java.util.Vector;
 
@@ -14,15 +55,20 @@ import java.util.Vector;
 
 @TeleOp(name = "bt_servoMovementRecord", group = "Iterative Opmode")
 // @Autonomous(...) is the other common choice
-@Disabled
+//@Disabled
 public class bt_servoMovementRecord extends OpMode {
     //Declare OpMode members.
     private ElapsedTime runtime = new ElapsedTime();
 
     Servo servo;
     TouchSensor sensor;
+    Context context;
+    File file;
+    File file2;
+    File file3;
 
     //Vector save = new Vector();
+    //File save;
 
     //Code to run ONCE when the driver hits INIT
     @Override
@@ -40,6 +86,38 @@ public class bt_servoMovementRecord extends OpMode {
 
         servo = hardwareMap.servo.get("servo");
         sensor = hardwareMap.touchSensor.get("sensor");
+
+        //save = new File(context.getFilesDir(), "save.txt");
+
+        context = hardwareMap.appContext;
+
+        //file = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS) + "/Servo Test Saves");
+        //if (!file.mkdir()) {
+        //    telemetry.addData("Error", "It didn't work, try again...");
+        //}
+
+        file = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS) + "/Servo Test Saves", "congrats.txt");
+        try {
+            file.createNewFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+            telemetry.addData("Error", "Maybe not congrats...");
+        }
+
+        try {
+            FileOutputStream f = new FileOutputStream(file);
+            PrintWriter pw = new PrintWriter(f);
+            pw.println("YAY! IT WORKED!");
+            pw.flush();
+            pw.close();
+            f.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            telemetry.addData("Error", "Maybe not congrats...");
+        } catch (IOException e) {
+            e.printStackTrace();
+            telemetry.addData("Error", "Maybe not congrats...");
+        }
 
     }
 
