1713e10d0d356b773cea0f662cf63c77f8fbc152
C987engineer <c987engineer@gmail.com>
2017-06-26 19:05:21 -0400

    This may seem crazy!
    Don't worry. Cleaning up for next year. Will post all old code in a
    different repo.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/archive/season2016_2017/all_opmodes_120916/AutoBlueOpMode.java
+++
@@ -1,9 +0,0 @@
-package org.firstinspires.ftc.teamcode.archive.season2016_2017.all_opmodes_120916;
-
-
-public class AutoBlueOpMode extends AutoBaseOpMode {
-
-    public int getFirstForwardTime() {
-        return 2000;
-    }
-}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/archive/season2016_2017/all_opmodes_120916/CameraPreview.java
+++
@@ -1,7 +0,0 @@
-package org.firstinspires.ftc.teamcode.archive.season2016_2017.all_opmodes_120916;
-
-/**
- * Created by tamaratershakovec1 on 9/18/16.
- */
-public class CameraPreview {
-}
